d740a07a0d9c415ea0122495b63bbd90d1b048fb
Madses <k.l.m.aartsen@hotmail.com>
2021-10-03 22:14:54 +0200

    added menu reworked container

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,5 +2,9 @@ import React from "react";
 import * as S from "./Menu.styled";
 
 export default function Menu() {
-    return <S.MenuContainer></S.MenuContainer>;
+    return (
+        <S.MenuContainer>
+            <S.Menu></S.Menu>
+        </S.MenuContainer>
+    );
 }
--- src/components/Menu/Menu.styled.js
+++ src/components/Menu/Menu.styled.js
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
 export const MenuContainer = styled.div`
-    width: 70px;
+    width: 100px;
     height: 100%;
-    background: #181818;
     position: fixed;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     z-index: 10;
+`;
+
+export const Menu = styled.div`
+    width: 70px;
+    height: 900px;
+    background: #181818;
+    border-radius: 20px;
+    align-self: center;
 `;
