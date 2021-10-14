08448a8f37b89b1fe7fb31867c486616eb0fc9aa
Madses <k.l.m.aartsen@hotmail.com>
2021-10-14 14:59:35 +0200

    added memoji

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -42,6 +42,8 @@ export const TextArea = styled.div`
 export const ImageContainer = styled.div`
     width: 50%;
     height: 100%;
+    text-align: center;
+
     img {
         height: 600px;
         width: auto;
